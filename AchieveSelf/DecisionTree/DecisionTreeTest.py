6f835fd8655903d5a0a0b63b82f188ba125482c0
jiangbaojian <qingdaopijui@sina.com>
2018-11-26 20:10:51 +0800

    2018年11月26日 Decision tree id3

--- AchieveSelf/DecisionTree/DecisionTreeTest.py
+++ AchieveSelf/DecisionTree/DecisionTreeTest.py
@@ -2,6 +2,7 @@
 #__author__: 'Baojian Jiang'
 #__time__:'2018/11/23'
 import numpy as np
+import operator
 class DecisionTree:
     """
     决策树的练习
@@ -22,38 +23,37 @@ class DecisionTree:
                 dataset_flag[i[-1]] += 1
         entropy = 0.0
         for i in dataset_flag.items():
-            # print(i[1])
             entropy += i[1] / dataset_len * np.log2(i[1]/dataset_len)
         return -entropy
 
     def createDataSet(self):
-        # dataset = [[1, 1, 'yes'],
-        #            [1, 1, 'yes'],
-        #            [1, 0, 'no'],
-        #            [0, 1, 'no'],
-        #            [0, 1, 'no']]
-        # labels = ['no surfacing', 'flippers']
-        dataset = [['青绿', '蜷缩', '浊响', '清晰', '凹陷', '硬滑', '好瓜'],
-            ['乌黑', '蜷缩', '沉闷', '清晰', '凹陷', '硬滑', '好瓜'],
-            ['乌黑', '蜷缩', '浊响', '清晰', '凹陷', '硬滑', '好瓜'],
-            ['青绿', '蜷缩', '沉闷', '清晰', '凹陷', '硬滑', '好瓜'],
-            ['浅白', '蜷缩', '浊响', '清晰', '凹陷', '硬滑', '好瓜'],
-            ['青绿', '稍蜷', '浊响', '清晰', '稍凹', '软粘', '好瓜'],
-            ['乌黑', '稍蜷', '浊响', '稍糊', '稍凹', '软粘', '好瓜'],
-            ['乌黑', '稍蜷', '浊响', '清晰', '稍凹', '硬滑', '好瓜'],
-            ['乌黑', '稍蜷', '沉闷', '稍糊', '稍凹', '硬滑', '坏瓜'],
-            ['青绿', '硬挺', '清脆', '清晰', '平坦', '软粘', '坏瓜'],
-            ['浅白', '硬挺', '清脆', '模糊', '平坦', '硬滑', '坏瓜'],
-            ['浅白', '蜷缩', '浊响', '模糊', '平坦', '软粘', '坏瓜'],
-            ['青绿', '稍蜷', '浊响', '稍糊', '凹陷', '硬滑', '坏瓜'],
-            ['浅白', '稍蜷', '沉闷', '稍糊', '凹陷', '硬滑', '坏瓜'],
-            ['乌黑', '稍蜷', '浊响', '清晰', '稍凹', '软粘', '坏瓜'],
-            ['浅白', '蜷缩', '浊响', '模糊', '平坦', '硬滑', '坏瓜'],
-            ['青绿', '蜷缩', '沉闷', '稍糊', '稍凹', '硬滑', '坏瓜']
-        ]
-
-        # 特征值列表
-        labels = ['色泽', '根蒂', '敲击', '纹理', '脐部', '触感']
+        dataset = [[1, 1, 'yes'],
+                   [1, 1, 'yes'],
+                   [1, 0, 'no'],
+                   [0, 1, 'no'],
+                   [0, 1, 'no']]
+        labels = ['no surfacing', 'flippers']
+        # dataset = [['青绿', '蜷缩', '浊响', '清晰', '凹陷', '硬滑', '好瓜'],
+        #     ['乌黑', '蜷缩', '沉闷', '清晰', '凹陷', '硬滑', '好瓜'],
+        #     ['乌黑', '蜷缩', '浊响', '清晰', '凹陷', '硬滑', '好瓜'],
+        #     ['青绿', '蜷缩', '沉闷', '清晰', '凹陷', '硬滑', '好瓜'],
+        #     ['浅白', '蜷缩', '浊响', '清晰', '凹陷', '硬滑', '好瓜'],
+        #     ['青绿', '稍蜷', '浊响', '清晰', '稍凹', '软粘', '好瓜'],
+        #     ['乌黑', '稍蜷', '浊响', '稍糊', '稍凹', '软粘', '好瓜'],
+        #     ['乌黑', '稍蜷', '浊响', '清晰', '稍凹', '硬滑', '好瓜'],
+        #     ['乌黑', '稍蜷', '沉闷', '稍糊', '稍凹', '硬滑', '坏瓜'],
+        #     ['青绿', '硬挺', '清脆', '清晰', '平坦', '软粘', '坏瓜'],
+        #     ['浅白', '硬挺', '清脆', '模糊', '平坦', '硬滑', '坏瓜'],
+        #     ['浅白', '蜷缩', '浊响', '模糊', '平坦', '软粘', '坏瓜'],
+        #     ['青绿', '稍蜷', '浊响', '稍糊', '凹陷', '硬滑', '坏瓜'],
+        #     ['浅白', '稍蜷', '沉闷', '稍糊', '凹陷', '硬滑', '坏瓜'],
+        #     ['乌黑', '稍蜷', '浊响', '清晰', '稍凹', '软粘', '坏瓜'],
+        #     ['浅白', '蜷缩', '浊响', '模糊', '平坦', '硬滑', '坏瓜'],
+        #     ['青绿', '蜷缩', '沉闷', '稍糊', '稍凹', '硬滑', '坏瓜']
+        # ]
+        #
+        # # 特征值列表
+        # labels = ['色泽', '根蒂', '敲击', '纹理', '脐部', '触感']
         return dataset, labels
 
     def chooseBestFeature(self, dataset):
@@ -74,7 +74,7 @@ class DecisionTree:
                 prob = len(sub_dataset) / float(dataset_length)
                 branch_entropy += prob * self.calcEntropy(sub_dataset)
             infor_gain[character] = root_entropy - branch_entropy
-        print("Entropy for each characters:", infor_gain)
+        # print("Entropy for each characters:", infor_gain)
         best_brahch = 0
         gain = 0.0
         for i in range(1, len(infor_gain)):
@@ -83,6 +83,46 @@ class DecisionTree:
                 gain = infor_gain[i]
         return best_brahch, gain
 
+    def majorityCnt(self, classList):
+        classCount = {}
+        for vote in classList:
+            if vote not in classCount.keys():
+                classCount[vote] = 0
+            classCount[vote] += 1
+        sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1), reverse=True)
+        return sortedClassCount[0][0]
+
+    def vote(self, classification):
+        classification_count = {}
+        for v in classification:
+            if v not in classification_count.keys():
+                classification_count[v] = 0
+            classification_count[v] += 1
+        sorted_Classification = sorted(classification_count.items(), key=operator.itemgetter(1), reverse=True)
+        return sorted_Classification[0][0]
+
+    def createTree(self, dataset, labels):
+        classification = [i[-1] for i in dataset]
+        if len(set(classification)) == 1:#样本全部属于同一类别
+            return classification[0]
+        # 如果 属性集A非空 或者 D中的样本在A中的取值相同
+        if len(dataset[0]) == 1:#样本中没有属性
+            return self.vote(classification)
+        best_branch, gain = self.chooseBestFeature(dataset)#属性下标
+        # print(best_branch)
+        best_branch_label = labels[best_branch]
+        tree = {best_branch_label: {}}
+        # print(tree)
+        # del(labels[best_branch])
+        np.delete(labels, best_branch)
+        #找出所有的属性值创建分支
+        characters = [i[best_branch] for i in dataset]
+        unique_characters = set(characters)
+        for value in unique_characters:
+            sub_labels = labels[:]
+            sub_dataset = [i for i in dataset if i[best_branch] == value]
+            tree[best_branch_label][value] = self.createTree(sub_dataset, sub_labels)
+        return tree
 
     def splitDataSet(self, dataset, axis, value):
         retDataSet = []
@@ -92,13 +132,19 @@ class DecisionTree:
                 reduceFeatVec.extend(featVec[axis+1:])
                 retDataSet.append(reduceFeatVec)
         return retDataSet
+
+
 if __name__  == '__main__':
     dt = DecisionTree()
     dataset, labels = dt.createDataSet()
     dataset = np.array(dataset)
-    entropy = dt.calcEntropy(dataset) #获取信息熵
-    print('计算信息熵：', entropy)
-    best_branch, gain = dt.chooseBestFeature(dataset)
-    print(best_branch, gain)
+    labels = np.array(labels)
+    # entropy = dt.calcEntropy(dataset) #获取信息熵
+    # print('计算信息熵：', entropy)
+    # best_branch, gain = dt.chooseBestFeature(dataset)
+    # print(best_branch, gain)
+    tree = dt.createTree(dataset, labels)
+    print(tree)
+
 
 
